c92fd9546c41215976c12231f15d6a71d337f30a
F4NT0 <gabriel.stundner@acad.pucrs.br>
2023-03-24 11:58:13 -0300

    Add project files.

---
+++ ProjetoCursoCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // significa que é desnecessário porque está transparente
+
+namespace ProjetoCursoCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
